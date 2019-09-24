d96232dba6a406cea5af527ce7cc045912c99c07
indigoparadox <indigo.repo@interfinitydynamics.info>
2019-09-24 15:00:51 -0400

    Rough start listening and shut down.

--- mkserv.c
+++ mkserv.c
@@ -6,6 +6,7 @@
 #include <linux/in.h>
 #include <net/net_namespace.h>
 #include <linux/mutex.h>
+#include <linux/kthread.h>
 
 #include "mkserv.h"
 
@@ -15,8 +16,11 @@ MODULE_DESCRIPTION( "A common TCP server framework for the kernel." );
 MODULE_VERSION( "0.19.9" );
 
 EXPORT_SYMBOL( mkserv_listen );
+EXPORT_SYMBOL( mkserv_shutdown );
 
-int mkserv_listen( struct socket** listen_sock_ptr, int port ) {
+struct mkservice testsvc = { 0 };
+
+int mkserv_listen( struct mkservice* service, int port ) {
    int res = 0;
    struct sockaddr_in listen_addr = { 0 };
 
@@ -25,8 +29,7 @@ int mkserv_listen( struct socket** listen_sock_ptr, int port ) {
    allow_signal( SIGKILL | SIGTERM );
 
    /* Create the listener socket. */
-   res = sock_create_kern(
-      &init_net, PF_INET, SOCK_STREAM, IPPROTO_TCP, listen_sock_ptr );
+   res = sock_create( AF_INET, SOCK_STREAM, IPPROTO_TCP, &(service->listener) );
 
    if( 0 > res ) {
       printk( KERN_ERR "unable to open mkserv socket\n" );
@@ -38,17 +41,51 @@ int mkserv_listen( struct socket** listen_sock_ptr, int port ) {
    listen_addr.sin_family = AF_INET;
    listen_addr.sin_port = htons( port );
 
-   res = (*listen_sock_ptr)->ops->bind( (*listen_sock_ptr),
+   res = service->listener->ops->bind( service->listener,
       (struct sockaddr*)&listen_addr, sizeof( listen_addr ) );
 
    if( 0 > res ) {
       printk( KERN_ERR "unable to bind socket to port %d\n", port );
+      mkserv_shutdown( service );
       goto cleanup;
+   }
+
+   /* Start listening. */
+   res = service->listener->ops->listen( service->listener, MKSERV_BACKLOG );
+
+   if( 0 > res ) {
+      printk( KERN_ERR "unable to listen\n" );
+      mkserv_shutdown( service );
+      goto cleanup;
+   }
+
+   while( 1 ) {
+      wait_event_timeout( wq, 0, 3 * HZ );
+
+      if( kthread_should_stop() ) {
+         res = 1;
+         printk( KERN_INFO "stopping listen thread...\n" );
+         goto cleanup;
+      }
+
+      if( signal_pending( current ) ) {
+         res = 1;
+         goto cleanup;
+      }
    }
 
 cleanup:
 
    return res;
+}
+
+int mkserv_shutdown( struct mkservice* service ) {
+   if( NULL != service->listener ) {
+      sock_release( service->listener );
+      service->listener = NULL;
+   }
+
+   return 0;
 }
 
 static int __init mkserv_init( void ) {
@@ -59,6 +96,8 @@ static int __init mkserv_init( void ) {
    res = sock_create_kern(
       &init_net, PF_INET, SOCK_STREAM, IPPROTO_TCP, &listener );
 */
+
+   mkserv_listen( &testsvc, 4567 );
 
    return 0;
 }
@@ -75,6 +114,8 @@ static void __exit mkserv_exit( void ) {
    printk( KERN_INFO "mkserv: closing socket...\n" );
    listener->ops->release( listener );
    listener = NULL;*/
+
+   mkserv_shutdown( &testsvc );
 }
 
 module_init( mkserv_init );
--- mkserv.h
+++ mkserv.h
@@ -2,7 +2,14 @@
 #ifndef MKSERV_H
 #define MKSERV_H
 
-int mkserv_listen( struct socket** listen_sock_ptr, int port );
+#define MKSERV_BACKLOG 16
+
+struct mkservice {
+   struct socket* listener;
+};
+
+int mkserv_listen( struct mkservice* service, int port );
+int mkserv_shutdown( struct mkservice* service );
 
 #endif /* MKSERV_H */
 
